fabfb8854470d1311682fbf34a72df472fbd50e9
lukasz_gro <lukaszgrobelczyk@gmail.com>
2021-05-25 13:24:53 +0200

    fight update

--- src/sample/Items.java
+++ src/sample/Items.java
@@ -4,7 +4,9 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.File;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Items {
     public int id;
@@ -13,6 +15,7 @@ public class Items {
         //0-Empty, 1-Boots, 2-Armor, 3-Helmet, 4-Weapon-one, 5-weapon-two, 6-trinket, 7-utility
         EMPTY, BOOTS, ARMOR, HELMET, WEAPON_ONE, WEAPON_TWO, TRINKET, UTILITY
     }
+    itemStats myStatistics;
 
     public Items(int types, int id) {
         this.id = id;
@@ -33,6 +36,7 @@ public class Items {
         } else if(types == 7){
             myType = type.UTILITY;
         }
+        myStatistics = new itemStats(types, id);
     }
     @Override
     public String toString(){
@@ -51,5 +55,55 @@ public class Items {
             imageView.setViewport(new Rectangle2D(64,64*10,64,64));
         }
         return imageView;
+    }
+
+    public class itemStats{
+        public String name;
+        public int attack;
+        public int defense;
+        public int luck;
+        public int agility;
+        public int hp;
+        public int mana;
+        public int stamina;
+        public itemStats(int x, int y){
+            String data = findLine(x, y);
+            if (data.equals("")){
+                return;
+            }
+            String[] tab_data = data.split(":", 11);
+            this.name = tab_data[2];
+            this.attack = Integer.parseInt(tab_data[3]);
+            this.defense = Integer.parseInt(tab_data[4]);
+            this.luck = Integer.parseInt(tab_data[5]);
+            this.agility = Integer.parseInt(tab_data[6]);
+            this.hp = Integer.parseInt(tab_data[7]);
+            this.mana = Integer.parseInt(tab_data[8]);
+            this.stamina = Integer.parseInt(tab_data[9]);
+        }
+
+        String findLine(int x, int y){
+            try {
+                File myObj = new File("src/resources/textures/Items/itemsDB.txt");
+                Scanner myReader = new Scanner(myObj);
+                String data = myReader.nextLine();
+                while(myReader.hasNextLine()){
+                    data = myReader.nextLine();
+                    String[] tmp_tab = data.split(":", 20);
+                    if(Integer.parseInt(tmp_tab[0]) == x && Integer.parseInt(tmp_tab[1])==y){
+                        return data;
+                    }
+                }
+            } catch (Exception e){
+                System.out.println("File has not been found - "+ this.toString() + ".txt");
+                e.printStackTrace();
+            }
+            return "";
+        }
+
+        @Override
+        public String toString(){
+            return "";
+        }
     }
 }
