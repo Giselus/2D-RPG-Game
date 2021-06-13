6ac08df329393bf5f86ce2a766e0f2fbeaddf0d1
Giselus <giselus.com@gmail.com>
2021-06-13 13:09:40 +0200

    Npc shops initial commit

--- src/sample/ContainerForNpc.java
+++ src/sample/ContainerForNpc.java
@@ -1,16 +1,29 @@
 package sample;
 
+import java.util.HashMap;
+
 public class ContainerForNpc{
+    public String name;
     public Inventory inventory;
     boolean isShop;
-    typeOfObject myType;
-    public enum typeOfObject{
-        CONTAINER, NPC, ENEMY
-    }
-    ContainerForNpc(int x_coordinate, int y_coordinate, int sizeX, int sizeY, boolean isShop){
-        //super(x_coordinate, y_coordinate);
-        this.myType = typeOfObject.CONTAINER;
-       // this.isShop = isShop;
+
+    public static HashMap<String, ContainerForNpc> npcsInventories = new HashMap<>();
+
+    ContainerForNpc(String name,int sizeX, int sizeY, boolean isShop){
+        this.name = name;
+        this.isShop = isShop;
         inventory = new Inventory(sizeX, sizeY);
+        npcsInventories.put(name,this);
+    }
+
+    public ContainerForNpc FetchByName(String name){
+        if(npcsInventories.containsKey(name))
+            return npcsInventories.get(name);
+        return null;
+    }
+
+    public static void Initialize(){
+        new ContainerForNpc("AlchemistShop", 4,4,true);
+        new ContainerForNpc("ArmourerShop", 4,4,true);
     }
 }
